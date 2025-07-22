f4b66f7f88ae4a305f74dd63e598873e9bcd669c
Duc Nguyen <ducnnhe181905@fpt.edu.vn>
2025-07-22 11:33:21 +0700

    add view score for teacher

---
+++ Project_Prn/Project_Prn/Project_Prn/ResultOfStudent/StudentScoreWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Prn.Models;
+using System.Linq;
+using System.Windows;
+
+namespace Project_Prn.ResultOfStudent
+{
+    public partial class StudentScoreWindow : Window
+    {
+        private readonly User currentUser;
+
+        public StudentScoreWindow(User teacher)
+        {
+            InitializeComponent();
+            currentUser = teacher;
+            LoadScores();
+        }
+
+        private void LoadScores()
+        {
+            using var context = new Prngroup4Context();
+
+            var results = context.Results
+                .Where(r => r.Exam.Course.TeacherId == currentUser.UserId)
+                .Include(r => r.User) // Load tên sinh viên
+                .Include(r => r.Exam)
+                    .ThenInclude(e => e.Course) // Load tên khóa học
+                .OrderBy(r => r.User.FullName)
+                .ToList();
+
+            dgScores.ItemsSource = results;
+        }
+
+    }
+}
